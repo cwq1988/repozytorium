f9b4316bd6619d46c00f59c1e9ce169f6aa37158
cwq1988 <36318@studentwsei.pl>
2020-03-17 02:29:35 +0100

    2 galaz

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main() {
   cout << "Hello, World!" <<endl;
   cout << "GIT HUB"<< endl;
-  cout << "test";
+  cout << "test"<<endl;
+  cout << "Druga gałaz test"<<endl;
     return 0;
 }
